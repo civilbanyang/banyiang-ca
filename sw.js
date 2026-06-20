b9cf407312512a5bf2431fa128d5fee7249c0718
civilbanyang <civilbanyang@gmail.com>
2026-06-20 10:13:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@
 //  - Background sync ready
 // ================================================================
 
-const CACHE      = 'scam5-v27';
-const TILE_CACHE = 'scam5-tiles-v27';
+const CACHE      = 'scam5-v28';
+const TILE_CACHE = 'scam5-tiles-v28';
 
 const ASSETS = [
   '/banyiang-ca/',
